e3053a41327f320aae4f4b594825956ca7bd6191
VonDefiant <fgomez2017ig@gmail.com>
2023-10-27 14:42:23 -0600

    CREACION DE CUTSO

--- programa-mamalon-de-pagos/FRONTEND/crearcurso.Designer.cs
+++ programa-mamalon-de-pagos/FRONTEND/crearcurso.Designer.cs
@@ -141,6 +141,7 @@
             Margin = new Padding(3, 4, 3, 4);
             Name = "crearcurso";
             Text = "crearcurso";
+            Load += crearcurso_Load;
             ((System.ComponentModel.ISupportInitialize)dgvAgregar).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
