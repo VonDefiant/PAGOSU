c3c6b58078d4cf964183e58a0ba3433d74abdc3b
VonDefiant <91977851+VonDefiant@users.noreply.github.com>
2023-10-27 11:25:02 -0600

    Add files via upload

--- programa-mamalon-de-pagos/FRONTEND/crearestudiante.cs
+++ programa-mamalon-de-pagos/FRONTEND/crearestudiante.cs
@@ -14,11 +14,12 @@ namespace programa_mamalon_de_pagos.FRONTEND
 {
     public partial class crearestudiante : Form
     {
-
+        private EstudianteDAO EstudianteDAO;
         public crearestudiante()
         {
             InitializeComponent();
-
+            string dbPath = "Data Source=BACKEND/EXACTUS.db;Version=3;";
+            EstudianteDAO = new EstudianteDAO(dbPath);
         }
 
         private void nombreestuadiante_TextChanged(object sender, EventArgs e)
@@ -41,6 +42,11 @@ namespace programa_mamalon_de_pagos.FRONTEND
 
         }
 
+        private void FACULTAD_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
         private void NACIMIENTOESTUDIANTE_TextChanged(object sender, EventArgs e)
         {
 
@@ -56,115 +62,57 @@ namespace programa_mamalon_de_pagos.FRONTEND
 
         }
 
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
         private void SAVEBUTTON_Click(object sender, EventArgs e)
         {
+            // Recopila los datos del formulario
+            string nombreCompleto = nombreestuadiante.Text;
+            DateTime fechaDeNacimiento;
+            if (!DateTime.TryParse(NACIMIENTOESTUDIANTE.Text, out fechaDeNacimiento))
+            {
+                MessageBox.Show("Fecha de nacimiento no válida.");
+                return;
+            }
 
-        }
+            string carrera = CARRERA.Text;
+            string seccion = SECCIONESTUDIANTE.Text;
+            string correoElectronico = EMAILESTUDIANTE.Text;
+            string telefono = telefonoalumno.Text;
+            string jornada = textBox3.Text;
+            string institucion = INSTITUCION.Text;
+            string facultad = FACULTAD.Text;
 
-        private void crearestudiante_Load(object sender, EventArgs e)
-        {
+            // Genera el número de carnet automáticamente (puedes personalizar la lógica)
+            int carnet = int.Parse(GenerarNumeroCarnet());
 
-        }
+            // Crea una instancia de Estudiante
+            Estudiante nuevoEstudiante = new Estudiante(carnet, nombreCompleto, fechaDeNacimiento, carrera, seccion, correoElectronico, telefono, jornada, institucion, facultad);
 
-        private void GUARDAR_Click(object sender, EventArgs e)
-        {
+
             try
             {
-                // Recopila los datos ingresados por el usuario desde los controles del formulario
-                string nombreCompleto = nombreestuadiante.Text;
-                string correoElectronico = EMAILESTUDIANTE.Text;
-                string telefono = telefonoalumno.Text;
-                string seccion = SECCIONESTUDIANTE.Text;
-                string carreraGrado = Cbcarrera.Text;
-                string institucion = Cbinstitucion.Text;
-                string facultad = CbFacultad.Text;
-                string jornada = cbjornada.Text;
+                // Llama a la función para insertar el estudiante en la base de datos
+                EstudianteDAO.InsertarEstudiante(nuevoEstudiante);
 
-                // Crea una instancia de la clase Estudiante con los datos ingresados
-                Estudiante estudiante = new Estudiante(nombreCompleto, carreraGrado, seccion, correoElectronico, telefono, institucion, facultad, jornada);
-
-                // Llama al método InsertarEstudiante para guardar los datos en la base de datos
-                estudiante.InsertarEstudiante();
-
-                // Muestra un mensaje de éxito
-                MessageBox.Show("Estudiante insertado exitosamente.");
-
-                // Limpia los controles del formulario después de guardar
-                LimpiarControles();
+                // Puedes mostrar un mensaje de confirmación o realizar otras acciones después de guardar el estudiante
+                MessageBox.Show("Estudiante guardado con éxito.");
             }
             catch (Exception ex)
             {
-                // Maneja cualquier excepción que pueda ocurrir al insertar el estudiante
-                MessageBox.Show("Error al insertar el estudiante: " + ex.Message);
+                MessageBox.Show("Error al guardar el estudiante: " + ex.Message);
             }
-
-
         }
-
-        private void CbFacultad_SelectedIndexChanged(object sender, EventArgs e)
+        private string GenerarNumeroCarnet()
         {
-
-        }
-
-        private void cbjornada_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void crearestudiante_Load_1(object sender, EventArgs e)
-        {
-
-        }
-
-        private void Cbinstitucion_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
-        }
-        private void LimpiarControles()
-        {
-            // Este método limpia todos los controles del formulario
-            nombreestuadiante.Text = "";
-            EMAILESTUDIANTE.Text = "";
-            telefonoalumno.Text = "";
-            SECCIONESTUDIANTE.Text = "";
-            Cbcarrera.SelectedIndex = -1; // Reinicia la selección en el ComboBox
-            Cbinstitucion.SelectedIndex = -1; // Reinicia la selección en el ComboBox
-            CbFacultad.SelectedIndex = -1; // Reinicia la selección en el ComboBox
-            cbjornada.SelectedIndex = -1; // Reinicia la selección en el ComboBox
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            try
-            {
-                // Recopila los datos ingresados por el usuario desde los controles del formulario
-                string nombreCompleto = nombreestuadiante.Text;
-                string correoElectronico = EMAILESTUDIANTE.Text;
-                string telefono = telefonoalumno.Text;
-                string seccion = SECCIONESTUDIANTE.Text;
-                string carreraGrado = Cbcarrera.Text;
-                string institucion = Cbinstitucion.Text;
-                string facultad = CbFacultad.Text;
-                string jornada = cbjornada.Text;
-
-                // Crea una instancia de la clase Estudiante con los datos ingresados
-                Estudiante estudiante = new Estudiante(nombreCompleto, carreraGrado, seccion, correoElectronico, telefono, institucion, facultad, jornada);
-
-                // Llama al método InsertarEstudiante para guardar los datos en la base de datos
-                estudiante.InsertarEstudiante();
-
-                // Muestra un mensaje de éxito
-                MessageBox.Show("Estudiante insertado exitosamente.");
-
-                // Limpia los controles del formulario después de guardar
-                LimpiarControles();
-            }
-            catch (Exception ex)
-            {
-                // Maneja cualquier excepción que pueda ocurrir al insertar el estudiante
-                MessageBox.Show("Error al insertar el estudiante: " + ex.Message);
-            }
-
+            // Puedes personalizar la lógica para generar el número de carnet automáticamente
+            // Aquí se muestra un ejemplo simple:
+            Random random = new Random();
+            string carnet = random.Next(1000, 9999).ToString() + "-" + random.Next(10, 99).ToString() + "-" + random.Next(1000, 9999).ToString();
+            return carnet;
         }
     }
 }
