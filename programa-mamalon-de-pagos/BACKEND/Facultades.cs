2e527e04aaf7e306a9c9b55d7ce9c41830437134
VonDefiant <91977851+VonDefiant@users.noreply.github.com>
2023-10-27 19:32:29 -0600

    Desarollo2 (#37)
    * aa
    
    * se cambio el boton pagos
    
    * pull
    
    * a
    
    * a
    
    * Se cambio los datos mostrados en el data grib
    
    * Update actualizarcurso.cs
    
    * Update actualizarcurso.cs
    
    * Finalización del modulo cursos
    
    ---------
    
    Co-authored-by: Francisco <francisco2002salazar@gmail.com>

--- programa-mamalon-de-pagos/BACKEND/Facultades.cs
+++ programa-mamalon-de-pagos/BACKEND/Facultades.cs
@@ -3,22 +3,55 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SQLite;
+
 
 namespace programa_mamalon_de_pagos.BACKEND
 {
     public class Facultades
     {
-        //Propiedades
-        public string? IdFacultad { get; set; }
-        public string? Nombre { get; set; }
-        public string? Institucion { get; set; }
+        // Propiedades
+        public int IdFacultad { get; set; }
+        public string Nombre { get; set; }
 
-        //Constructor
-        public Facultades(string idFacultad, string nombre, string institucion)
+        // Constructor con dos argumentos
+        public Facultades(int idFacultad, string nombre)
         {
             IdFacultad = idFacultad;
             Nombre = nombre;
-            Institucion = institucion;
+        }
+
+        public void InsertarFacultad()
+        {
+            using (SQLiteConnection connection = new SQLiteConnection("Data Source=BACKEND/EXACTUS.db;Version=3;"))
+            {
+                connection.Open();
+
+                string queryLastIdFacultad = "SELECT MAX(IDFacultad) FROM Facultades;";
+                using (SQLiteCommand cmdLastIdInstitucion = new SQLiteCommand(queryLastIdFacultad, connection))
+                {
+                    var result = cmdLastIdInstitucion.ExecuteScalar();
+                    if (result != DBNull.Value)
+                    {
+                        IdFacultad = Convert.ToInt32(result) + 1;
+                    }
+                    else
+                    {
+                        IdFacultad = 1;
+                    }
+                }
+
+                string insertQuery = "INSERT INTO Facultades (IDFacultad, Nombre) " +
+                                    "VALUES (@IDFacultad, @Nombre);";
+
+                using (SQLiteCommand cmd = new SQLiteCommand(insertQuery, connection))
+                {
+                    cmd.Parameters.AddWithValue("@IDFacultad", IdFacultad);
+                    cmd.Parameters.AddWithValue("@Nombre", Nombre);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
         }
     }
 }
